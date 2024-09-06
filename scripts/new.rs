0fe708a07071e1266267c2ed333e96a4604435e3
Kaylee Simmons <kay@the-simmons.net>
2024-09-06 15:26:13 -0700

    Add project argument to new script

--- scripts/new.rs
+++ scripts/new.rs
@@ -1,14 +1,31 @@
 //! ```cargo
 //! [dependencies]
 //! chrono = "0.4.24"
+//! clap = { version = "4.5.17", features = ["derive"] }
 //! convert_case = "0.6.0"
+//! indoc = "2"
 //! ```
 
 use std::io::Write;
 
+use clap::Parser;
 use convert_case::{Case, Casing};
+use indoc::formatdoc;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The title for today's blog post
+    title: String,
+
+    /// The project to link this blog post to
+    #[arg(short, long, value_name = "PROJECT_NAME")]
+    project: Option<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+
     // Go to ./content/trio/maple/ and find all the files and directories that
     // start with "day##-"
     let mut days: Vec<String> = Vec::new();
@@ -36,7 +53,7 @@ fn main() {
     // Create the new day name using the passed in commandline argument
     // as the suffix
     let day_number = max + 1;
-    let title = std::env::args().nth(1).unwrap().to_case(Case::Kebab);
+    let title = args.title.to_case(Case::Kebab);
     let new_day = format!("day{day_number}-{title}");
 
     // Create a new directory for the new day and create an index.md file
@@ -54,17 +71,27 @@ fn main() {
     std::fs::create_dir(&new_day_path).unwrap();
     let index_path = format!("{new_day_path}/index.md");
     let title = title.to_case(Case::Title);
+
+    let extra = if let Some(project) = args.project.filter(|p| !p.is_empty()) {
+        formatdoc! {r#"
+
+            [extra]
+            project = "{project}"
+        "#}
+    } else {
+        String::new()
+    };
+
     std::fs::write(
         index_path.clone(),
-        format!(
-            r#"+++
-title = "Day{day_number} - {title}"
-description = ""
-date = {date}
-+++
+        formatdoc! {r#"
+            +++
+            title = "Day{day_number} - {title}"
+            description = ""
+            date = {date}
+            {extra}+++
 
-"#,
-        ),
+        "#},
     );
 
     // Print out the index.md file path so that the user can open it in
